40a01eb0147523e666e2c088ed6f3e6448371565
sam bassett <samson.bassett@gmail.com>
2015-07-27 14:56:57 -0700

    PowerApples: adding config file to control each item, as requested on forums

--- src/main/java/com/lothrazar/samsapples/ItemRegistry.java
+++ src/main/java/com/lothrazar/samsapples/ItemRegistry.java
@@ -19,8 +19,27 @@ public class ItemRegistry
 
 	public static int hunger = 4; 
 	public static int time = 8 * 60;// 8:00
-   
-	 
+
+	public static boolean apple_emerald_enabled;
+	public static boolean apple_diamond_enabled; 
+	public static boolean apple_ender_enabled; 
+	public static boolean apple_bone_enabled; 
+	public static boolean apple_lapis_enabled; 
+	public static boolean apple_chocolate_enabled;
+	public static boolean apple_netherwart_enabled; 
+	public static boolean apple_prismarine_enabled;
+	public static boolean apple_clownfish_enabled;
+
+	public static boolean apple_emerald_expensive;
+	public static boolean apple_diamond_expensive; 
+	public static boolean apple_ender_expensive; 
+	public static boolean apple_bone_expensive; 
+	public static boolean apple_lapis_expensive; 
+	public static boolean apple_chocolate_expensive;
+	public static boolean apple_netherwart_expensive; 
+	public static boolean apple_prismarine_expensive;
+	public static boolean apple_clownfish_expensive;
+	
 	public static ItemFoodAppleMagic apple_emerald;
 	public static ItemFoodAppleMagic apple_diamond; 
 	public static ItemFoodAppleMagic apple_ender; 
@@ -31,74 +50,97 @@ public class ItemRegistry
 	public static ItemFoodAppleMagic apple_prismarine;
 	public static ItemFoodAppleMagic apple_slowfall;
 	
-  public static final int clownfish = 2;
+  private static final int clownfish = 2;
 
 	public static final int dye_cocoa = 3;
 	public static final int dye_lapis = 4;
 
 	public static void registerItems()
 	{   
-		//TODO: config file for each item??
-		ItemRegistry.apple_ender = new ItemFoodAppleMagic(hunger, false);
-		ItemRegistry.apple_ender.addEffect(PotionRegistry.ender.id, time, I);
-		ItemRegistry.registerItem(ItemRegistry.apple_ender, "apple_ender");
+		if(apple_ender_enabled)
+		{
+			ItemRegistry.apple_ender = new ItemFoodAppleMagic(hunger, false);
+			ItemRegistry.apple_ender.addEffect(PotionRegistry.ender.id, time, I);
+			ItemRegistry.registerItem(ItemRegistry.apple_ender, "apple_ender");
+	
+			ItemFoodAppleMagic.addRecipe(ItemRegistry.apple_ender,new ItemStack(Items.ender_pearl));
+		}
+		
+		if(apple_emerald_enabled)
+		{
+			ItemRegistry.apple_emerald = new ItemFoodAppleMagic(hunger, false);
+			ItemRegistry.apple_emerald.addEffect(Potion.moveSpeed.id, time, II);  
+			ItemRegistry.apple_emerald.addEffect(Potion.absorption.id, time, I);  
+			ItemRegistry.apple_emerald.addEffect(Potion.saturation.id, time, I); 
+			ItemRegistry.registerItem(ItemRegistry.apple_emerald, "apple_emerald");
+			ItemFoodAppleMagic.addRecipe(ItemRegistry.apple_emerald,new ItemStack(Items.emerald));
+		}
+		
+		if(apple_chocolate_enabled)
+		{
+			ItemRegistry.apple_chocolate = new ItemFoodAppleMagic(hunger, false); 
+			ItemRegistry.apple_chocolate.addEffect(Potion.weakness.id, time, I);
+			ItemRegistry.apple_chocolate.addEffect(Potion.moveSpeed.id, time, I); 
+			ItemRegistry.registerItem(ItemRegistry.apple_chocolate, "apple_chocolate");
+			ItemFoodAppleMagic.addRecipe(ItemRegistry.apple_chocolate, new ItemStack(Items.dye, 1, dye_cocoa) );
+		}
+		
+		if(apple_lapis_enabled)
+		{
+			ItemRegistry.apple_lapis = new ItemFoodAppleMagic(hunger, false); 
+			ItemRegistry.apple_lapis.addEffect(Potion.digSpeed.id, time, II); //Haste
+			ItemRegistry.registerItem(ItemRegistry.apple_lapis, "apple_lapis");
+			ItemFoodAppleMagic.addRecipe(ItemRegistry.apple_lapis, new ItemStack(Items.dye, 1, dye_lapis) );
+		}
+		if(apple_diamond_enabled)
+		{
+			ItemRegistry.apple_diamond = new ItemFoodAppleMagic(hunger, false);
+			ItemRegistry.registerItem(ItemRegistry.apple_diamond, "apple_diamond");
+			ItemRegistry.apple_diamond.addEffect(Potion.resistance.id, time, I); 
+			ItemRegistry.apple_diamond.addEffect(Potion.healthBoost.id, time, V); 
+			ItemRegistry.apple_diamond.addEffect(Potion.saturation.id, time, I); 
+			GameRegistry.addShapelessRecipe(new ItemStack(ItemRegistry.apple_diamond)
+				, new ItemStack(Items.diamond)
+				, new ItemStack(Items.apple));
+			GameRegistry.addSmelting(apple_diamond, new ItemStack(Items.diamond, 1),	0);
+		}
+		
+		if(apple_bone_enabled)
+		{
+			apple_bone = new ItemFoodAppleMagic(hunger, false);
+			ItemRegistry.registerItem(ItemRegistry.apple_bone, "apple_bone");
+			ItemRegistry.apple_bone.addEffect(PotionRegistry.nav.id, time, I); 
+			ItemFoodAppleMagic.addRecipe(ItemRegistry.apple_bone,new ItemStack(Items.bone));
+		}
+		
+		if(apple_netherwart_enabled)
+		{
+			apple_netherwart = new ItemFoodAppleMagic(hunger, false);
+			ItemRegistry.registerItem(ItemRegistry.apple_netherwart, "apple_netherwart");
+			ItemRegistry.apple_netherwart.addEffect(Potion.digSlowdown.id, time, I); //Mining Fatigue
+			ItemRegistry.apple_netherwart.addEffect(Potion.waterBreathing.id, time, I);  
+			ItemFoodAppleMagic.addRecipe(ItemRegistry.apple_netherwart,new ItemStack(Items.nether_wart));
+		}
+		
+		if(apple_prismarine_enabled)
+		{
+			apple_prismarine = new ItemFoodAppleMagic(hunger, false);
+			ItemRegistry.registerItem(ItemRegistry.apple_prismarine, "apple_prismarine");
+			ItemRegistry.apple_prismarine.addEffect(PotionRegistry.waterwalk.id, time, I); 
+			ItemFoodAppleMagic.addRecipe(ItemRegistry.apple_prismarine,new ItemStack(Items.prismarine_shard));
+		}
 
-		ItemFoodAppleMagic.addRecipe(ItemRegistry.apple_ender,new ItemStack(Items.ender_pearl));
+		if(apple_clownfish_enabled)
+		{
+			apple_slowfall = new ItemFoodAppleMagic(hunger, false);
+			ItemRegistry.registerItem(ItemRegistry.apple_slowfall, "apple_slowfall");
+			ItemRegistry.apple_slowfall.addEffect(PotionRegistry.slowfall.id, time, I); 
+			//ItemFoodAppleMagic.addRecipe(ItemRegistry.apple_slowfall,new ItemStack(Items.feather));
 	 
-		ItemRegistry.apple_emerald = new ItemFoodAppleMagic(hunger, false);
-		ItemRegistry.apple_emerald.addEffect(Potion.moveSpeed.id, time, II);  
-		ItemRegistry.apple_emerald.addEffect(Potion.absorption.id, time, I);  
-		ItemRegistry.apple_emerald.addEffect(Potion.saturation.id, time, I); 
-		ItemRegistry.registerItem(ItemRegistry.apple_emerald, "apple_emerald");
-		ItemFoodAppleMagic.addRecipe(ItemRegistry.apple_emerald,new ItemStack(Items.emerald));
-
-		ItemRegistry.apple_chocolate = new ItemFoodAppleMagic(hunger, false); 
-		ItemRegistry.apple_chocolate.addEffect(Potion.weakness.id, time, I);
-		ItemRegistry.apple_chocolate.addEffect(Potion.moveSpeed.id, time, I); 
-		ItemRegistry.registerItem(ItemRegistry.apple_chocolate, "apple_chocolate");
-		ItemFoodAppleMagic.addRecipe(ItemRegistry.apple_chocolate, new ItemStack(Items.dye, 1, dye_cocoa) );
-
-		ItemRegistry.apple_lapis = new ItemFoodAppleMagic(hunger, false); 
-		ItemRegistry.apple_lapis.addEffect(Potion.digSpeed.id, time, II); //Haste
-		ItemRegistry.registerItem(ItemRegistry.apple_lapis, "apple_lapis");
-		ItemFoodAppleMagic.addRecipe(ItemRegistry.apple_lapis, new ItemStack(Items.dye, 1, dye_lapis) );
-		
-		ItemRegistry.apple_diamond = new ItemFoodAppleMagic(hunger, false);
-		ItemRegistry.registerItem(ItemRegistry.apple_diamond, "apple_diamond");
-		ItemRegistry.apple_diamond.addEffect(Potion.resistance.id, time, I); 
-		ItemRegistry.apple_diamond.addEffect(Potion.healthBoost.id, time, V); 
-		ItemRegistry.apple_diamond.addEffect(Potion.saturation.id, time, I); 
-		GameRegistry.addShapelessRecipe(new ItemStack(ItemRegistry.apple_diamond)
-			, new ItemStack(Items.diamond)
-			, new ItemStack(Items.apple));
-		GameRegistry.addSmelting(apple_diamond, new ItemStack(Items.diamond, 1),	0);
-
-		apple_bone = new ItemFoodAppleMagic(hunger, false);
-		ItemRegistry.registerItem(ItemRegistry.apple_bone, "apple_bone");
-		ItemRegistry.apple_bone.addEffect(PotionRegistry.nav.id, time, I); 
-		ItemFoodAppleMagic.addRecipe(ItemRegistry.apple_bone,new ItemStack(Items.bone));
-		
-		apple_netherwart = new ItemFoodAppleMagic(hunger, false);
-		ItemRegistry.registerItem(ItemRegistry.apple_netherwart, "apple_netherwart");
-		ItemRegistry.apple_netherwart.addEffect(Potion.digSlowdown.id, time, I); //Mining Fatigue
-		ItemRegistry.apple_netherwart.addEffect(Potion.waterBreathing.id, time, I);  
-		ItemFoodAppleMagic.addRecipe(ItemRegistry.apple_netherwart,new ItemStack(Items.nether_wart));
-
-		apple_prismarine = new ItemFoodAppleMagic(hunger, false);
-		ItemRegistry.registerItem(ItemRegistry.apple_prismarine, "apple_prismarine");
-		ItemRegistry.apple_prismarine.addEffect(PotionRegistry.waterwalk.id, time, I); 
-		ItemFoodAppleMagic.addRecipe(ItemRegistry.apple_prismarine,new ItemStack(Items.prismarine_shard));
-		
-
-		apple_slowfall = new ItemFoodAppleMagic(hunger, false);
-		ItemRegistry.registerItem(ItemRegistry.apple_slowfall, "apple_slowfall");
-		ItemRegistry.apple_slowfall.addEffect(PotionRegistry.slowfall.id, time, I); 
-		//ItemFoodAppleMagic.addRecipe(ItemRegistry.apple_slowfall,new ItemStack(Items.feather));
- 
-		GameRegistry.addShapelessRecipe(new ItemStack(ItemRegistry.apple_slowfall)
-			, new ItemStack(Items.apple)
-			, new ItemStack(Items.fish,1,clownfish)); 
-		
+			GameRegistry.addShapelessRecipe(new ItemStack(ItemRegistry.apple_slowfall)
+				, new ItemStack(Items.apple)
+				, new ItemStack(Items.fish,1,clownfish)); 
+		}
 	}
 	
 	public static void registerItem(Item item, String name)
--- src/main/java/com/lothrazar/samsapples/ModApples.java
+++ src/main/java/com/lothrazar/samsapples/ModApples.java
@@ -3,6 +3,7 @@ package com.lothrazar.samsapples;
 import java.util.Random;
 
 import org.apache.logging.log4j.Logger;   
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.client.renderer.Tessellator;
@@ -25,6 +26,7 @@ import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.event.entity.living.EnderTeleportEvent;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
 import net.minecraftforge.fml.common.FMLCommonHandler;
@@ -63,11 +65,58 @@ public class ModApples
 	public void onPreInit(FMLPreInitializationEvent event)
 	{ 
 		logger = event.getModLog();  
+		String category = Configuration.CATEGORY_GENERAL;
 		
-		//cfg = new ConfigRegistry(new Configuration(event.getSuggestedConfigurationFile()));
-		/*	slowfallSpeed = instance.getFloat("slowfall_speed",category, 0.41F,0.1F,1F,
-		"This factor affects how much the slowfall effect slows down the entity.");
-*/
+		Configuration cfg = new Configuration(event.getSuggestedConfigurationFile());
+		cfg.load();
+		
+
+		ItemRegistry.apple_bone_enabled = cfg.get( category, "apple_bone_enabled",true).getBoolean();
+		ItemRegistry.apple_emerald_enabled = cfg.get( category, "apple_emerald_enabled",true).getBoolean();
+		ItemRegistry.apple_diamond_enabled = cfg.get( category, "apple_diamond_enabled",true).getBoolean();
+		ItemRegistry.apple_ender_enabled = cfg.get( category, "apple_ender_enabled",true).getBoolean();
+		ItemRegistry.apple_lapis_enabled = cfg.get( category, "apple_lapis_enabled",true).getBoolean();
+		ItemRegistry.apple_chocolate_enabled = cfg.get( category, "apple_chocolate_enabled",true).getBoolean();
+		ItemRegistry.apple_netherwart_enabled = cfg.get( category, "apple_netherwart_enabled",true).getBoolean();
+		ItemRegistry.apple_prismarine_enabled = cfg.get( category, "apple_prismarine_enabled",true).getBoolean();
+		ItemRegistry.apple_clownfish_enabled = cfg.get( category, "apple_clownfish_enabled",true).getBoolean();
+		
+
+		category = "recipes";
+		cfg.addCustomCategoryComment(category, 
+				"True means you have to fully surround the apple with 8 items, false means only a single item will craft with the red apple.");
+
+		ItemRegistry.apple_bone_expensive = cfg.get( category, "apple_bone_expensive",true).getBoolean();
+		ItemRegistry.apple_emerald_expensive = cfg.get( category, "apple_emerald_expensive",true).getBoolean();
+		ItemRegistry.apple_diamond_expensive = cfg.get( category, "apple_diamond_expensive",false).getBoolean();
+		ItemRegistry.apple_ender_expensive = cfg.get( category, "apple_ender_expensive",true).getBoolean();
+		ItemRegistry.apple_lapis_expensive = cfg.get( category, "apple_lapis_expensive",true).getBoolean();
+		ItemRegistry.apple_chocolate_expensive = cfg.get( category, "apple_chocolate_expensive",true).getBoolean();
+		ItemRegistry.apple_netherwart_expensive = cfg.get( category, "apple_netherwart_expensive",true).getBoolean();
+		ItemRegistry.apple_prismarine_expensive = cfg.get( category, "apple_prismarine_expensive",true).getBoolean();
+		ItemRegistry.apple_clownfish_expensive = cfg.get( category, "apple_clownfish_expensive",false).getBoolean();
+		
+		
+		
+		category = "effect_tweaks";
+		
+		PotionRegistry.slowfallSpeed = cfg.getFloat("slowfall_speed",category, 0.41F,0.1F,1F,
+				"This factor affects how much the slowfall effect slows down the entity.");
+
+		category = "effect_ids";
+
+		cfg.addCustomCategoryComment(category, 
+				"IDs are only exposed to avoid conflicts with other mods.  Messing with these might break the game.   ");
+
+		
+		
+		PotionRegistry.ender_id = cfg.get(category,"ender_id", 50).getInt();
+		PotionRegistry.nav_id = cfg.get(category,"nav_id", 51).getInt();
+		PotionRegistry.waterwalk_id = cfg.get(category,"waterwalk_id", 52).getInt(); 
+		PotionRegistry.slowfall_id = cfg.get(category,"slowfall_id", 53).getInt();
+		
+		if(cfg.hasChanged()){cfg.save();}
+		
     	//network = NetworkRegistry.INSTANCE.newSimpleChannel( Reference.MODID );     	
     	
     	//network.registerMessage(MessageKeyPressed.class, MessageKeyPressed.class, MessageKeyPressed.ID, Side.SERVER);
